5a9ee850b095bec070bc4c7f4846c1d1be5ddfdf
William <williamsuane@hotmail.com>
2017-02-02 15:35:35 -0500

    aula 199

--- src/br/com/abc/javacore/ZZMcompletablefuture/test/LojaTest2.java
+++ src/br/com/abc/javacore/ZZMcompletablefuture/test/LojaTest2.java
@@ -4,6 +4,8 @@ import br.com.abc.javacore.ZZMcompletablefuture.classes.Loja;
 
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 import java.util.stream.Collectors;
 
 import static java.util.Arrays.asList;
@@ -24,14 +26,119 @@ public class LojaTest2 {
                 new Loja("casas bahia"),
                 new Loja("bestbuy"),
                 new Loja("casas bahia"),
+                new Loja("casas bahia"),
+                new Loja("bestbuy"),
+                new Loja("casas bahia"),
+                new Loja("bestbuy"),
+                new Loja("casas bahia"),
+                new Loja("casas bahia"),
+                new Loja("bestbuy"),
+                new Loja("casas bahia"),
+                new Loja("bestbuy"),
+                new Loja("casas bahia"),
+                new Loja("casas bahia"),
+                new Loja("bestbuy"),
+                new Loja("casas bahia"),
+                new Loja("bestbuy"),
+                new Loja("casas bahia"),
+                new Loja("casas bahia"),
+                new Loja("bestbuy"),
+                new Loja("casas bahia"),
+                new Loja("bestbuy"),
+                new Loja("casas bahia"),
+                new Loja("casas bahia"),
+                new Loja("bestbuy"),
+                new Loja("casas bahia"),
+                new Loja("bestbuy"),
+                new Loja("casas bahia"),
+                new Loja("casas bahia"),
+                new Loja("bestbuy"),
+                new Loja("casas bahia"),
+                new Loja("bestbuy"),
+                new Loja("casas bahia"),
+                new Loja("casas bahia"),
+                new Loja("bestbuy"),
+                new Loja("casas bahia"),
+                new Loja("bestbuy"),
+                new Loja("casas bahia"),
+                new Loja("casas bahia"),
+                new Loja("bestbuy"),
+                new Loja("casas bahia"),
+                new Loja("bestbuy"),
+                new Loja("casas bahia"),
+                new Loja("casas bahia"),
+                new Loja("bestbuy"),
+                new Loja("casas bahia"),
+                new Loja("bestbuy"),
+                new Loja("casas bahia"),
+                new Loja("casas bahia"),
+                new Loja("bestbuy"),
+                new Loja("casas bahia"),
+                new Loja("bestbuy"),
+                new Loja("casas bahia"),
+                new Loja("casas bahia"),
+                new Loja("bestbuy"),
+                new Loja("casas bahia"),
+                new Loja("bestbuy"),
+                new Loja("casas bahia"),
+                new Loja("casas bahia"),
+                new Loja("bestbuy"),
+                new Loja("casas bahia"),
+                new Loja("bestbuy"),
+                new Loja("casas bahia"),
+                new Loja("casas bahia"),
+                new Loja("bestbuy"),
+                new Loja("casas bahia"),
+                new Loja("bestbuy"),
+                new Loja("casas bahia"),
+                new Loja("casas bahia"),
+                new Loja("bestbuy"),
+                new Loja("casas bahia"),
+                new Loja("bestbuy"),
+                new Loja("casas bahia"),
+                new Loja("casas bahia"),
+                new Loja("bestbuy"),
+                new Loja("casas bahia"),
+                new Loja("bestbuy"),
+                new Loja("casas bahia"),
+                new Loja("casas bahia"),
+                new Loja("bestbuy"),
+                new Loja("casas bahia"),
+                new Loja("bestbuy"),
+                new Loja("casas bahia"),
+                new Loja("casas bahia"),
+                new Loja("bestbuy"),
+                new Loja("casas bahia"),
+                new Loja("bestbuy"),
+                new Loja("casas bahia"),
+                new Loja("casas bahia"),
+                new Loja("bestbuy"),
+                new Loja("casas bahia"),
+                new Loja("bestbuy"),
+                new Loja("casas bahia"),
                 new Loja("bestbuy"),
                 new Loja("casas bahia"),
                 new Loja("bestbuy"),
                 new Loja("wallmart"));
+        System.out.println(Runtime.getRuntime().availableProcessors());
+        System.out.println(lojas.size());
 //        acharPrecos(lojas);
         acharPrecos2(lojas);
 //        acharPrecos3(lojas);
         acharPrecos4(lojas);
+        // NumeroDeThreads = Ncpu * Ucpu * (1+W/C)
+        // Ncpu = numero de cores disponivel
+        // Ucpu = quantidade de utilização da CPU (0-1)
+        // W/C = wait time e compute time
+        // Ntrheads = 8 * 1 * (1+99) = 800 threads
+        final Executor executor = Executors.newFixedThreadPool(Math.min(lojas.size(), 100), r -> {
+            Thread t = new Thread(r);
+            t.setDaemon(true);
+            return t;
+        });
+        acharPrecos4(lojas,executor);
+
+
     }
 
     private static List<String> acharPrecos(List<Loja> lojas) {
@@ -79,6 +186,25 @@ public class LojaTest2 {
                 .map(loja -> CompletableFuture.supplyAsync(
                         () -> String.format("%s o preco eh: %.2f", loja.getNome(), loja.getPreco())
                 ))
+                .collect(Collectors.toList());
+        System.out.println("Tempo de invocacao: " + (System.currentTimeMillis() - start) + " ms");
+
+        List<String> collect = precoFuturo.stream().map(CompletableFuture::join).collect(Collectors.toList());
+        System.out.println("Tempo total: " + (System.currentTimeMillis() - start) + " ms");
+
+        System.out.println(collect);
+        return collect;
+    }
+
+    private static List<String> acharPrecos4(List<Loja> lojas, Executor executor) {
+        System.out.println("Completable Future ");
+        long start = System.currentTimeMillis();
+
+        List<CompletableFuture<String>> precoFuturo = lojas.stream()
+                .map(loja -> CompletableFuture
+                        .supplyAsync(() -> String.format("%s o preco eh: %.2f", loja.getNome(),
+                                loja.getPreco()), executor
+                        ))
                 .collect(Collectors.toList());
         System.out.println("Tempo de invocacao: " + (System.currentTimeMillis() - start) + " ms");
 
